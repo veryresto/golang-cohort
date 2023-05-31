46897da334aa6f3b8eadedd4965b35dc36dd9868
veryresto <veryresto@gmail.com>
2023-05-31 19:58:58 +0700

    add changes from pertemuan 4

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -2,6 +2,8 @@ package utils
 
 import (
 	"math/rand"
+
+	"gorm.io/gorm"
 )
 
 func RandString(length int) string {
@@ -14,4 +16,27 @@ func RandString(length int) string {
 	}
 
 	return string(b)
+}
+
+func Paginate(offset int, limit int) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		page := offset
+
+		// Jika page isinya kurang atau sama dengan 0 kita akan ganti menjadi 1
+		if page <= 0 {
+			page = 1
+		}
+
+		pageSize := limit
+
+		switch {
+		case pageSize > 100:
+			pageSize = 100
+		case pageSize <= 0:
+			pageSize = 10
+		}
+
+		offset := (page - 1) * limit
+		return db.Offset(offset).Limit(pageSize)
+	}
 }
