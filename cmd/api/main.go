7cb5a55387a63b5abd350557bdead70aa45c232c
veryresto <veryresto@gmail.com>
2023-05-31 20:04:37 +0700

    add changes from pertemuan 4

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 
 	mysql "online-course/pkg/db/mysql"
 
+	admin "online-course/internal/admin/injector"
 	forgotPassword "online-course/internal/forgot_password/injector"
 	oauth "online-course/internal/oauth/injector"
 	register "online-course/internal/register/injector"
@@ -20,6 +21,7 @@ func main() {
 	oauth.InitializedService(db).Route(&r.RouterGroup)
 	verificationEmail.InitializedService(db).Route(&r.RouterGroup)
 	forgotPassword.InitializedService(db).Route(&r.RouterGroup)
+	admin.InitializedService(db).Route(&r.RouterGroup)
 
 	r.Run()
 }
