5cbfb1d603ebc185810c12e23bd84cd5b9ab0bc3
veryresto <veryresto@gmail.com>
2023-06-04 14:40:22 +0700

    pertemuan 7: update main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,16 +3,18 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 
-	mysql "online-course/pkg/db/mysql"
+	mysql "online-course.faerul.com/pkg/db/mysql"
 
-	admin "online-course/internal/admin/injector"
-	discount "online-course/internal/discount/injector"
-	forgotPassword "online-course/internal/forgot_password/injector"
-	oauth "online-course/internal/oauth/injector"
-	product "online-course/internal/product/injector"
-	productCategory "online-course/internal/product_category/injector"
-	register "online-course/internal/register/injector"
-	verificationEmail "online-course/internal/verification_email/injector"
+	admin "online-course.faerul.com/internal/admin/injector"
+	cart "online-course.faerul.com/internal/cart/injector"
+	discount "online-course.faerul.com/internal/discount/injector"
+	forgotPassword "online-course.faerul.com/internal/forgot_password/injector"
+	oauth "online-course.faerul.com/internal/oauth/injector"
+	order "online-course.faerul.com/internal/order/injector"
+	product "online-course.faerul.com/internal/product/injector"
+	productCategory "online-course.faerul.com/internal/product_category/injector"
+	register "online-course.faerul.com/internal/register/injector"
+	verificationEmail "online-course.faerul.com/internal/verification_email/injector"
 )
 
 func main() {
@@ -28,6 +30,8 @@ func main() {
 	productCategory.InitializedService(db).Route(&r.RouterGroup)
 	product.InitializedService(db).Route(&r.RouterGroup)
 	discount.InitializedService(db).Route(&r.RouterGroup)
+	cart.InitializedService(db).Route(&r.RouterGroup)
+	order.InitializedService(db).Route(&r.RouterGroup)
 
 	r.Run()
 }
